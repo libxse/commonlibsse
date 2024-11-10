6cc67610c74c906962766b761aaae45f4d68b2b5
Qudix <17361645+Qudix@users.noreply.github.com>
2024-11-09 22:23:30 -0600

    fix: move includes

--- include/SKSE/Impl/PCH.h
+++ include/SKSE/Impl/PCH.h
@@ -58,14 +58,14 @@ static_assert(
 	std::is_integral_v<std::time_t> && sizeof(std::time_t) == sizeof(std::size_t),
 	"wrap std::time_t instead");
 
+#include "REX/REX.h"
+#include "REX/W32/KERNEL32.h"
+#include "REX/W32/USER32.h"
+
 #pragma warning(push)
 #include <binary_io/file_stream.hpp>
 #include <spdlog/spdlog.h>
 #pragma warning(pop)
-
-#include "REX/REX.h"
-#include "REX/W32/KERNEL32.h"
-#include "REX/W32/USER32.h"
 
 namespace SKSE
 {
