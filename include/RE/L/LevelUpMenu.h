5c58e082d5fd0e25f5f792a62b59f19d5933fb0e
Talles <115373190+digital-apple@users.noreply.github.com>
2024-12-05 19:50:34 -0300

    Update LevelUpMenu.h

--- include/RE/L/LevelUpMenu.h
+++ include/RE/L/LevelUpMenu.h
@@ -20,10 +20,10 @@ namespace RE
 		void Accept(CallbackProcessor* a_cbReg) override;  // 01
 
 		// members
-		bool          unk30;  // 30
-		std::uint8_t  pad31;  // 31
-		std::uint16_t pad32;  // 32
-		std::uint32_t pad34;  // 34
+		bool          playerLeveled;  // 30
+		std::uint8_t  pad31;          // 31
+		std::uint16_t pad32;          // 32
+		std::uint32_t pad34;          // 34
 	};
 	static_assert(sizeof(LevelUpMenu) == 0x38);
 }
