7af36f3b539019d14e44624d8d995118b6454f0b
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2026-08-28 08:57:09 -0600

    chore: bump game version number

--- include/SKSE/Version.h
+++ include/SKSE/Version.h
@@ -34,6 +34,7 @@ namespace SKSE
 	inline constexpr REL::Version RUNTIME_1_6_1170(1, 6, 1170, 0);
 	inline constexpr REL::Version RUNTIME_1_6_1179(1, 6, 1179, 0);
 	inline constexpr REL::Version RUNTIME_1_7_99(1, 7, 99, 0);
+	inline constexpr REL::Version RUNTIME_1_7_104(1, 7, 104, 0);
 
-	inline constexpr REL::Version RUNTIME_LATEST = RUNTIME_1_7_99;
+	inline constexpr REL::Version RUNTIME_LATEST = RUNTIME_1_7_104;
 }
